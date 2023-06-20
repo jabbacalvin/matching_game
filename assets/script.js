fb77648e93f6e96684f50618a3a07eebfc715bc4
Calvin Hsu <jabbacalvin@gmail.com>
2023-06-20 17:55:03 -0500

    added toggle compared items and compare value functionality

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,18 @@ let game;
 
 const categoryWrapperEl = document.querySelector('.categories');
 const categoryBtns = document.querySelectorAll('.categoryBtns');
+const gameContainerWrapperEl = document.querySelector('.gameContainer');
+const categoryItemsWrapperEl = document.querySelector('.categoryItems');
 const itemBtns = document.querySelectorAll('.itemBtns');
-const matchingBtns = document.querySelectorAll('.matchingBtns');
+const matchingItemsWrapperEl = document.querySelector('.matchingItems');
+const matchingItemBtns = document.querySelectorAll('.matchingItemBtns');
 const resetBtn = document.querySelector('.resetBtn');
+
+gameContainerWrapperEl.addEventListener('click', (e) => {
+    if (e.target.tagName.toLowerCase() === 'button') {
+        game.compareValues();
+    }
+});
 
 class Categories {
     static categoriesLookup = {
@@ -23,9 +32,15 @@ class Categories {
         this.render();
         this.domWrapperEl = domWrapperEl;
         this.domWrapperEl.addEventListener('click', (e) => {
-            this.category = e.target.innerText.toLowerCase();
-            new Options(itemBtns, this.category);
-            new Options(matchingBtns, this.category);
+            if (e.target.tagName.toLowerCase() === 'button') {
+                this.category = e.target.innerText.toLowerCase();
+                
+                categoryBtns.forEach(e => e.classList.remove('focusCategory'));
+                e.target.classList.toggle('focusCategory');
+
+                new Options(itemBtns, this.category);
+                new Options(matchingItemBtns, this.category);
+            }
         });
     }
     render() {
@@ -66,19 +81,51 @@ class Options {
 }
 
 class MatchingGame {
-    constructor(itemBtns, matchingBtns) {
+    constructor(categoryItemsWrapperEl, matchingItemsWrapperEl) {
+        this.categoryItemsWrapperEl = categoryItemsWrapperEl;
+        this.matchingItemsWrapperEl = matchingItemsWrapperEl;
+        this.itemBtnValue = null;
+        this.matchingItemBtnValue = null;
 
+        this.render();
+    }
+    displayClick(domWrapperEl) {
+        domWrapperEl.addEventListener('click', (e) => {
+            if (e.target.tagName.toLowerCase() === 'button' && e.target.hasAttribute('data-id')) {
+                const listArray = [...domWrapperEl.children];
+                listArray.forEach(e => e.classList.remove('focusMatches'));
+                e.target.classList.toggle('focusMatches');
+
+                const dataId = e.target.getAttribute('data-id');
+
+                if (e.target.classList.contains('itemBtns')) {
+                    this.itemBtnValue = dataId;
+                } else {
+                    this.matchingItemBtnValue = dataId;
+                }
+            }
+        });
+    }
+    compareValues() {      
+        this.itemBtnValue === this.matchingItemBtnValue ? console.log(true) : console.log(false);
+        this.matchingItemBtnValue === this.itemBtnValue ? console.log(true) : console.log(false);
     }
     play() {
 
     }
     render() {
-        
+        this.displayClick(this.categoryItemsWrapperEl);
+        this.displayClick(this.matchingItemsWrapperEl);
     }
 }
+
+categoryItemsWrapperEl.addEventListener('click', (e) => {
+
+})
 
 initialize();
 
 function initialize() {
     const categories = new Categories(categoryBtns, categoryWrapperEl);
+    game = new MatchingGame(categoryItemsWrapperEl, matchingItemsWrapperEl);
 }
